2e75556e007ae6fc3b7b727285b437a1449e1ed3
scottroot <allanscotthendrix@gmail.com>
2026-01-12 03:28:45 -0500

    update temporal env

--- temporal/worker.ts
+++ temporal/worker.ts
@@ -4,7 +4,7 @@ import * as activities from './activities';
 
 async function run() {
   const connection = await NativeConnection.connect({
-    address: process.env.TEMPORAL_URL,
+    address: process.env.TEMPORAL_ADDRESS,
   });
 
   const worker = await Worker.create({
@@ -16,7 +16,7 @@ async function run() {
   });
 
   console.log('Worker started on task queue: content-generation');
-  console.log('Connected to Temporal server at:', connection.options.address);
+  // console.log('Connected to Temporal server at:', connection.options.address);
 
   await worker.run();
 }
