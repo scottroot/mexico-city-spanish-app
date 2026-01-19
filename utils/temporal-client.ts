438c7c025192ff4069cd4f537e1cceacbeb84b91
scottroot <allanscotthendrix@gmail.com>
2026-01-19 14:15:24 -0600

    update cron schedule

--- utils/temporal-client.ts
+++ utils/temporal-client.ts
@@ -2,11 +2,20 @@ import { Connection, Client } from '@temporalio/client';
 
 
 export async function getTemporalClient() {
+  const address = process.env.TEMPORAL_ADDRESS;
+
+  if (!address) {
+    throw new Error('TEMPORAL_ADDRESS environment variable is not set');
+  }
+
+  console.log('Connecting to Temporal server at:', address);
+
   const connection = await Connection.connect({
-    address: process.env.TEMPORAL_ADDRESS,
+    address,
+    connectTimeout: 10000, // 10 seconds for Vercel cold starts
   });
 
-  console.log('Connected to Temporal server at:', connection.options.address);
+  console.log('Connected to Temporal server');
 
   return new Client({ connection });
 }
